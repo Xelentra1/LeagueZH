cac5341a31900466b9c4f6df339eb6a7afd6ff2f
siyarbekir <51085143+siyarbekir@users.noreply.github.com>
2020-01-05 03:04:18 +0100

    Update Neu ZH richtig.cpp

--- Neu ZH richtig/Neu ZH richtig.cpp
+++ Neu ZH richtig/Neu ZH richtig.cpp
@@ -23,19 +23,19 @@ int main()
 	else
 	{
 		uintptr_t modulBase = GetModuleBaseAddress(procId, L"League of Legends.exe");
-		HANDLE hProcess{0};
+		HANDLE hProcess{ 0 };
 		hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
 		uintptr_t dynamicPtrBaseAddrZoom = modulBase + 0x01AFA418;
 		uintptr_t dynamicPtrBaseAddrZoom2 = modulBase + 0x0339496C;
-		std::vector<unsigned int> ZoomOffsets = { 12 , 608 };
-		std::vector<unsigned int> ZoomOffsets2 = { 40 };
-		std::vector<unsigned int> FovOffsets = { 12 , 376 };
-		uintptr_t FovAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom, FovOffsets); 
+		//vector<unsigned int> ZoomOffsets = { 12 , 608 }; //not needen anymore was bugged and did zoom out without controlling it ingame
+		vector<unsigned int> ZoomOffsets2 = { 40 };
+		vector<unsigned int> FovOffsets = { 12 , 376 };
+		uintptr_t FovAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom, FovOffsets);
 		//uintptr_t ZoomAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom, ZoomOffsets); //outdated method
 		uintptr_t ZoomAddr2 = FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom2, ZoomOffsets2);
 		if (procId == NULL)
 		{
-			cout << "Fehler konnte ProcID nicht finden" << '\n';
+			cout << "seems like something went wrong" << '\n';
 			Sleep(4000);
 			exit(-1);
 		}
@@ -50,22 +50,22 @@ int main()
 					neuZoomValue = neuZoomValue + 50;
 					cout << neuZoomValue << endl;
 				}
-				if (GetAsyncKeyState(VK_SUBTRACT))
+				else if (GetAsyncKeyState(VK_SUBTRACT))
 				{
 					//WriteProcessMemory(hProcess, (LPVOID)ZoomAddr, &neuZoomValue, sizeof(neuZoomValue), 0); //outdated
 					WriteProcessMemory(hProcess, (LPVOID)ZoomAddr2, &neuZoomValue, sizeof(neuZoomValue), 0);
 					neuZoomValue = neuZoomValue - 50;
 					cout << neuZoomValue << endl;
 				}
-				if (GetAsyncKeyState(VK_UP))
+				else if (GetAsyncKeyState(VK_UP))
 				{
-					WriteProcessMemory(hProcess, (LPVOID)FovAddr, &neuFovValue, sizeof(neuFovValue), 0); 
+					WriteProcessMemory(hProcess, (LPVOID)FovAddr, &neuFovValue, sizeof(neuFovValue), 0);
 					neuFovValue = neuFovValue + 1;
 					cout << neuFovValue << endl;
 				}
-				if (GetAsyncKeyState(VK_DOWN))
+				else if (GetAsyncKeyState(VK_DOWN))
 				{
-					WriteProcessMemory(hProcess, (LPVOID)FovAddr, &neuFovValue, sizeof(neuFovValue), 0); 
+					WriteProcessMemory(hProcess, (LPVOID)FovAddr, &neuFovValue, sizeof(neuFovValue), 0);
 					neuFovValue = neuFovValue - 1;
 					cout << neuFovValue << endl;
 				}
