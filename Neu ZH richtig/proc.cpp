101c6b4bd969f862c8785b20e84adca458445c11
siyarbekir <51085143+siyarbekir@users.noreply.github.com>
2020-01-05 23:58:14 +0100

    Update proc.cpp

--- Neu ZH richtig/proc.cpp
+++ Neu ZH richtig/proc.cpp
@@ -8,7 +8,7 @@
 DWORD GetProcId(const wchar_t* procName)
 {
 	DWORD procId{ 0 };
-	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	HANDLE hSnap{ CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)};
 	if (hSnap != INVALID_HANDLE_VALUE)
 	{
 		PROCESSENTRY32 procEntry;
@@ -57,7 +57,7 @@ uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
 
 uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
 {
-	uintptr_t addr = ptr;
+	uintptr_t addr{ ptr };
 	for (unsigned int i = 0; i < offsets.size(); ++i)
 	{
 		ReadProcessMemory(hProc, (BYTE*)addr, &addr, sizeof(addr), 0);
