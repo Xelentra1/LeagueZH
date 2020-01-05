b85b82d9bcfcd1d1d3552e2218e8ccc4c99fd8fc
siyarbekir <51085143+siyarbekir@users.noreply.github.com>
2020-01-05 23:53:17 +0100

    Update Neu ZH richtig.cpp

--- Neu ZH richtig/Neu ZH richtig.cpp
+++ Neu ZH richtig/Neu ZH richtig.cpp
@@ -14,7 +14,7 @@ int main()
 	//These are the standart values for FOV and Zoom
 	float neuZoomValue{ 2250 };
 	float neuFovValue{ 40 };
-	DWORD procId{GetProcId(L"League Of Legends.exe")};
+	DWORD procId{ GetProcId(L"League Of Legends.exe") };
 	if (procId == NULL)
 	{
 		cout << "League need to be started" << '\n';
@@ -23,17 +23,17 @@ int main()
 	}
 	else
 	{
-		uintptr_t modulBase{GetModuleBaseAddress(procId, L"League of Legends.exe")};
+		uintptr_t modulBase{ GetModuleBaseAddress(procId, L"League of Legends.exe") };
 		HANDLE hProcess{};
 		hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
-		uintptr_t dynamicPtrBaseAddrZoom{ modulBase + 0x01AFA418};
-		uintptr_t dynamicPtrBaseAddrZoom2{ modulBase + 0x0339496C};
+		uintptr_t dynamicPtrBaseAddrZoom{ modulBase + 0x01AFA418 };
+		uintptr_t dynamicPtrBaseAddrZoom2{ modulBase + 0x0339496C };
 		//vector<unsigned int> ZoomOffsets = { 12 , 608 }; //not needen anymore was bugged and did zoom out without controlling it ingame
 		vector<unsigned int> ZoomOffsets2{ 40 };
 		vector<unsigned int> FovOffsets{ 12 , 376 };
-		uintptr_t FovAddr{ FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom, FovOffsets)};
+		uintptr_t FovAddr{ FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom, FovOffsets) };
 		//uintptr_t ZoomAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom, ZoomOffsets); //outdated method
-		uintptr_t ZoomAddr2{ FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom2, ZoomOffsets2)};
+		uintptr_t ZoomAddr2{ FindDMAAddy(hProcess, dynamicPtrBaseAddrZoom2, ZoomOffsets2) };
 		if (procId == NULL)
 		{
 			cout << "seems like something went wrong" << '\n';
@@ -49,26 +49,26 @@ int main()
 					//WriteProcessMemory(hProcess, (LPVOID)ZoomAddr, &neuZoomValue, sizeof(neuZoomValue), 0); //outdated
 					WriteProcessMemory(hProcess, (LPVOID)ZoomAddr2, &neuZoomValue, sizeof(neuZoomValue), 0);
 					neuZoomValue = neuZoomValue + 50;
-					cout << neuZoomValue +50 << '\n';
+					cout << neuZoomValue << '\n';
 				}
 				else if (GetAsyncKeyState(VK_SUBTRACT))
 				{
 					//WriteProcessMemory(hProcess, (LPVOID)ZoomAddr, &neuZoomValue, sizeof(neuZoomValue), 0); //outdated
 					WriteProcessMemory(hProcess, (LPVOID)ZoomAddr2, &neuZoomValue, sizeof(neuZoomValue), 0);
 					neuZoomValue = neuZoomValue - 50;
-					cout << neuZoomValue - 50 << '\n';
+					cout << neuZoomValue << '\n';
 				}
 				else if (GetAsyncKeyState(VK_UP))
 				{
 					WriteProcessMemory(hProcess, (LPVOID)FovAddr, &neuFovValue, sizeof(neuFovValue), 0);
 					neuFovValue = neuFovValue + 1;
-					cout << neuFovValue + 1 << '\n';
+					cout << neuFovValue << '\n';
 				}
 				else if (GetAsyncKeyState(VK_DOWN))
 				{
 					WriteProcessMemory(hProcess, (LPVOID)FovAddr, &neuFovValue, sizeof(neuFovValue), 0);
 					neuFovValue = neuFovValue - 1;
-					cout << neuFovValue - 1 << '\n';
+					cout << neuFovValue << '\n';
 				}
 				Sleep(100);
 			}while (true);
